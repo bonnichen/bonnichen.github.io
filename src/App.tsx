b8d320ff11fdf0c818a72224f253eb77dbda0b89
bonnichen <bonnie.chen1208@gmail.com>
2025-10-01 21:56:09 -0400

    init Website

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainPage from "./pages/MainPage";
+console.log("App is being imported!");
+function App() {
+  return (
+    <MainPage/>
+  );
+}
+
+export default App;

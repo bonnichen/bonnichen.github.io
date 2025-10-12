bb29f73297a2b7a135aa37f2e965580ef4672cd2
bonnichen <bonnie.chen1208@gmail.com>
2025-10-12 00:00:15 -0400

    Added an about me section and fixed formatting for mobile devices

---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,21 @@
+const AboutMe = () => {
+  return (
+    <div className="aboutContainer">
+    <div className="aboutMe">
+      <div>
+        <h3>About Me </h3>
+        <h4>
+          Hi, I'm Bonnie, a sophomore at Cornell University majoring in computer
+          science. I am passionate about creating impactful and accessible
+          solutions to real-world problems. world problems. I enjoy exploring
+          how technology can be used to create efficient, secure, and
+          user-centered systems that help make a positive difference in everyday
+          life.
+        </h4>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;

e2df9247e492cf324f4512db75d45ed6e8be3e86
bonnichen <bonnie.chen1208@gmail.com>
2025-10-12 22:54:54 -0400

    fixed formatting on mobile

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,11 +3,11 @@ import ProjectCard from "./Components/ProjectCard";
 const Projects = () => {
   return (
     <div className="projectContainer" id="project">
-      <h3 style={{marginBottom : "6vw"}}>Projects</h3>
+      <h3 style={{ marginBottom: "6vw" }}>Projects</h3>
       <ProjectCard
         title="Chess"
         image={{
-          src: "/images/chess.png",
+          src: "images/chess.png",
           alt: "game of chess where black is in play. Black has its bishop selected and yellow highlights on the board corresponds to all possible moves for the selected bishop.",
         }}
         description="A fully interactive chess application designed with a 
@@ -18,7 +18,7 @@ const Projects = () => {
       <ProjectCard
         title="Cash Flow"
         image={{
-          src: "/images/cashflowmainboard.png",
+          src: "images/cashflowmainboard.png",
           alt: "A overview of the users spendings. The users icome, spendings and savings are listed on the left. Their budgetting summary and a piechart visualizing the summary is on the right.",
         }}
         description="An interactive budgeting website that allows users to track their personal spending and plan for future investments."
